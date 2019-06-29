184d3882a67de92ba4eeb8f5f1a65df7286eb896
cshihlin9015 <cshihlin9015@gmail.com>
2019-06-29 23:14:43 +0800

    Lambda,Thread sleep

--- src/main/java/com/csl9015/thread/Tester.java
+++ src/main/java/com/csl9015/thread/Tester.java
@@ -12,6 +12,11 @@ public class Tester {
             public void run() {
                 for (int i = 0; i < 10; i++) {
                     System.out.println("thread: " + i);
+                    try {
+                        sleep(50);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         };
@@ -27,11 +32,37 @@ public class Tester {
             public void run() {
                 for (int i = 0; i < 10; i++) {
                     System.out.println("runnable: " + i);
+                    try {
+                        Thread.sleep(50);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         };
         Thread r = new Thread(runnable);
         r.start();
+
+        // Runable - Lambda
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                for (int i = 0; i < 10; i++) {
+//                    System.out.println("runnable - Lambda: " + i);
+//                }
+//            }
+//        }).start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("runnable - Lambda: " + i);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
 
         System.out.println("main end");
     }
@@ -42,6 +73,11 @@ class MyThread extends Thread {
     public void run() {
         for (int i = 0; i < 10; i++) {
             System.out.println(getName() + ":" + i);
+            try {
+                sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
