1d52faf548dc475b7c61a47d2ecba454a24080fa
cshihlin9015 <cshihlin9015@gmail.com>
2019-06-29 14:54:29 +0800

    FileInputStream,FileReader,BufferedReader

--- src/main/java/com/csl9015/io/Tester.java
+++ src/main/java/com/csl9015/io/Tester.java
@@ -1,22 +1,72 @@
 package com.csl9015.io;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 
 public class Tester {
     public static void main(String[] args) {
+//        inputstream();
+//        filereader();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("data2.txt"));
+            String line = br.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void inputstream() {
         File file = new File("data.txt");
         System.out.println(file.exists());
         System.out.println(file.getAbsolutePath());
-        try {
-            InputStream is = new FileInputStream(file);
-        }catch (FileNotFoundException e){
-            e.printStackTrace();
-            System.out.println("message");
-        }
-        System.out.println("File open success");
 
+        try {
+            FileInputStream is = new FileInputStream("data.txt");
+            int n = is.read();
+            while (n != -1) {
+                System.out.print((char) n);
+                n = is.read();
+            }
+            System.out.println();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//        try {
+//            InputStream is = new FileInputStream(file);
+//            int n = is.read();
+//            System.out.println(n);
+//            System.out.println(is.read());
+//            System.out.println(is.read());
+//            System.out.println(is.read());
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//            System.out.println("message");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        System.out.println("File open success");
+    }
+
+    private static void filereader() {
+        try {
+            FileReader fr = new FileReader("data2.txt");
+            int n = fr.read();
+            while (n != -1) {
+                System.out.print((char) n);
+                n = fr.read();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
