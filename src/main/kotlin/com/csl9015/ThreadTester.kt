fd61f5c1d0562c5672074b043e99338d2ca0fa77
cshihlin9015 <cshihlin9015@gmail.com>
2019-06-29 23:50:09 +0800

    Kotlin thread,Coroutines

---
+++ src/main/kotlin/com/csl9015/ThreadTester.kt
@@ -0,0 +1,27 @@
+package com.csl9015
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
+
+fun main(args: Array<String>) {
+    Thread({
+        for(i in 1..10){
+            println("Thread: ${i}")
+            Thread.sleep(50)
+        }
+    }).start()
+
+    thread {
+        for(i in 1..10){
+        println("thread(): ${i}")
+        Thread.sleep(50)
+    } }
+
+    GlobalScope.launch {
+        for(i in 1..10){
+        println("coroutines: ${i}")
+        delay(50L)
+    } }
+}
